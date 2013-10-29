ce3c2ef2b749543a9f11e4af68f9b52f9d2be6c4
Ethan <uhrerq@gmail.com>
2013-10-30 02:14:26 +0800

    remove build

--- Build/Intermediates/iBeaconsTutorial.build/Debug-iphonesimulator/iBeaconsTutorial.build/Objects-normal/i386/IBTViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/iisscc/cloud/scm/github/Objective-C/iBeaconsTutorial/iBeaconsTutorial/IBTViewController.m \
-  /Users/iisscc/cloud/scm/github/Objective-C/iBeaconsTutorial/iBeaconsTutorial/IBTViewController.h
--- Build/Intermediates/iBeaconsTutorial.build/Debug-iphonesimulator/iBeaconsTutorial.build/Objects-normal/i386/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/iisscc/cloud/scm/github/Objective-C/iBeaconsTutorial/iBeaconsTutorial/main.m \
-  /Users/iisscc/cloud/scm/github/Objective-C/iBeaconsTutorial/iBeaconsTutorial/IBTAppDelegate.h
